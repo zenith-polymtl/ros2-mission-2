dcd096dde601a411e4266b3ec221a50d8d3d0188
SeyliB <ilbelhaddad@outlook.com>
2025-03-21 16:29:27 -0400

    modif approach

--- mission/mission/approach.py
+++ mission/mission/approach.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import TwistStamped, PoseStamped
 from std_msgs.msg import String
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 import time
-
+# Ilyes est trop cool
 class PIDController:
     def __init__(self, kp, ki, kd, max_output=2.0):
         self.kp = kp
